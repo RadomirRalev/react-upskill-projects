8453459371a29e44a3d42d9950eb86af3afc50b9
Radomir Ralev <rado_ralev@yahoo.com>
2020-11-05 22:51:16 +0200

    16

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ReactRootPlugin = require("html-webpack-root-plugin");
-
 
 module.exports = {
   entry: "./src/index.js",
@@ -40,6 +38,5 @@ module.exports = {
       "ReactDOM": "react-dom"
     }),
     new HtmlWebpackPlugin(),
-    new ReactRootPlugin()
   ]
 };
