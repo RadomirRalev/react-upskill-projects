36b16708f94852ecda006df076d27e8cea953aeb
Radomir Ralev <rado_ralev@yahoo.com>
2020-11-10 19:34:37 +0200

    4165415

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
   resolve: { extensions: ["*", ".js", ".jsx"] },
   output: {
     path: path.resolve(__dirname, "dist/"),
-    publicPath: "./",
+    publicPath: "/dist/",
     filename: "bundle.js"
   },
   devServer: {
