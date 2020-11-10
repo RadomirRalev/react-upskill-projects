80f49ba81a1319e8662eee25dacfef0e26da09f5
Radomir Ralev <rado_ralev@yahoo.com>
2020-11-10 19:16:17 +0200

    dsad

--- webpack.config.js
+++ webpack.config.js
@@ -44,8 +44,7 @@ module.exports = {
     }),
     new CopyPlugin({
       patterns: [
-        { from: "source", to: "dest" },
-        { from: "other", to: "public" },
+        { from: "public", to: "/" }
       ],
     }),
       
