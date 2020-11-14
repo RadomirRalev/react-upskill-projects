1ccb2b08110d2e9461f109cc37d01ccba06e3dbc
Radomir Ralev <rado_ralev@yahoo.com>
2020-11-14 15:02:34 +0200

    6561

---
+++ build-utils/webpack.common.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: ["react-hot-loader/patch", "./src/index.js"],
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /(node_modules|bower_components)/,
+          loader: "babel-loader",
+          options: { presets: ["@babel/env"] }
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"]
+        }
+      ]
+    },
+    resolve: { extensions: ["*", ".js", ".jsx"] },
+    output: {
+      path: path.resolve(__dirname, "dist/"),
+      publicPath: "/react-upskill-projects/",
+      filename: "bundle.js"
+    },
+    devServer: {
+      contentBase: path.join(__dirname, "public/"),
+      port: 3000,
+      publicPath: "http://localhost:3000/dist/",
+      hotOnly: true
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.ProvidePlugin({
+        "React": "react",
+        "ReactDOM": "react-dom"
+      }),
+      new HtmlWebpackPlugin({
+        template: "indexcopy.html",
+      })
+    ]
+  };
