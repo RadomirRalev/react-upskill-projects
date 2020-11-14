c7de986bb45a24b1b372772ac9577e9cd64ac7ae
Radomir Ralev <rado_ralev@yahoo.com>
2020-11-14 13:45:00 +0200

    651651

--- src/ButtonWithHook.js
+++ src/ButtonWithHook.js
@@ -4,7 +4,7 @@ function ButtonWithHook() {
     const [count, setCount] = useState(0);
 
     return (
-    <button onClick={() => setCount(count + 1)}>This is the hook count {count}</button>
+    <button onClick={() => setCount(count + 1)}>Functional component with useState Hook {count}</button>
     );
 }
 
